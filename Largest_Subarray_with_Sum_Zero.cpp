c1caaa0ee5909acdae0b228a11371296c54b8299
AryanRana12345 <aryan.rana.ug24@nsut.ac.in>
2026-07-01 16:23:25 +0530

    I fixed the bug discussed in previous message also i found out that the starting allocation of sum 0 was also correct and at that point while writing that it was not in my mind that i have to use index to find the length of the subarray

--- Largest_Subarray_with_Sum_Zero.cpp
+++ Largest_Subarray_with_Sum_Zero.cpp
@@ -1,18 +1,21 @@
+//{6, -2, 2, -8, 1, 7, 4, -10} 
 #include<iostream>
 #include<vector>
-#include<map>
+#include<unordered_map>
 using namespace std;
 int Largest_Subarray_with_Sum_Zero(int n, vector<int> arr){
-    map<int,int> hash_map;
+    unordered_map<int,int> hash_map;
     int length = 0;
-    hash_map[0] = 1;
+    hash_map[0] = -1;
     int sum = 0;
     for(int i=0;i<n;i++){
         sum = sum + arr[i];
         if (hash_map.find(sum) != hash_map.end()){
             length = max(length,i - hash_map[sum]);
         }
-        hash_map[sum] = i;
+        else{
+            hash_map[sum] = i;
+        }
     }
     return length;
 }
