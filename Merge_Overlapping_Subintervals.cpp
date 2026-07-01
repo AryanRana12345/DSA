fc6b2d0c7a988926ebe5d6854ec9fa2ea0b288e3
AryanRana12345 <aryan.rana.ug24@nsut.ac.in>
2026-07-01 19:47:27 +0530

    Here my code is wrong as in out i am just getting the brackets but no value inside them like this '[[][][]]' also before that i also found out an error in the for loop condition but i myself figured it out.

---
+++ Merge_Overlapping_Subintervals.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<vector<int>> Merge_Ovarlapping_Subintervals(vector<vector<int>> arr){
+    sort(arr.begin(),arr.end());
+    vector<vector<int>> output;
+    int start = arr[0][0];
+    int end = arr[0][1];
+    //[[1,3],[2,6],[8,10],[15,18]]
+    for(int i = 0; i < arr.size()-1; i++){
+        if(end >= arr[i+1][0]){
+            end = arr[i+1][1];
+        }else{
+            output.push_back({start,end});
+            start = arr[i+1][0];
+            end = arr[i+1][1];
+        }
+    }
+    output.push_back({start,end});
+    return output;
+}
+int main(){
+    vector<vector<int>> input = {{1,3},{2,6},{8,10},{15,18}};
+    vector<vector<int>> output = Merge_Ovarlapping_Subintervals(input);
+    cout << "[";
+    for(vector<int> interval : output){
+        cout << "[";
+        for(int i = 0;i = 0;i++){
+            cout << interval[i] << "," << interval[i+1];
+        }
+        cout << "]";
+    }
+    cout << "]"; 
+}
