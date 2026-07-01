a7af5cdd5e5739f091fc487f678317ee4ca6f39f
AryanRana12345 <aryan.rana.ug24@nsut.ac.in>
2026-07-01 20:34:08 +0530

    The code which i commited previously was not the final version of the solution of this problem as my code was not taking care of the edge case where just [] is provided as input so now at the start i have added a guard clause which returns [] back if this is the input

--- Merge_Overlapping_Subintervals.cpp
+++ Merge_Overlapping_Subintervals.cpp
@@ -3,11 +3,13 @@
 #include<algorithm>
 using namespace std;
 vector<vector<int>> Merge_Ovarlapping_Subintervals(vector<vector<int>> arr){
+    if(arr.empty()){
+        return {};
+    }
     sort(arr.begin(),arr.end());
     vector<vector<int>> output;
     int start = arr[0][0];
     int end = arr[0][1];
-    //[[1,3],[2,6],[8,10],[15,18]]
     for(int i = 0; i < arr.size()-1; i++){
         if(end >= arr[i+1][0]){
             end = max(end,arr[i+1][1]);
@@ -21,7 +23,7 @@ vector<vector<int>> Merge_Ovarlapping_Subintervals(vector<vector<int>> arr){
     return output;
 }
 int main(){
-    vector<vector<int>> input = {{1,3},{2,6},{8,10},{15,18}};
+    vector<vector<int>> input = {};
     vector<vector<int>> output = Merge_Ovarlapping_Subintervals(input);
     cout << "[";
     for(vector<int> interval : output){
