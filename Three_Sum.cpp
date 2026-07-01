b2f62991c442f2fec9600f1178854749bafbcc5f
AryanRana12345 <aryan.rana.ug24@nsut.ac.in>
2026-07-01 14:07:57 +0530

    Here I by mistake skipped to include the avoiding same value element twice condition for the outer for loop and only included them for inner loops leading to wrong output

--- Three_Sum.cpp
+++ Three_Sum.cpp
@@ -6,16 +6,38 @@ vector<vector<int>> Three_Sum(int n, vector<int> arr){
     sort(arr.begin(),arr.end());
     vector<vector<int>> temp;
     for(int i=0;i<n;i++){
+        if (i > 0 && arr[i-1] == arr[i]){
+            continue;
+        }
         int j = i+1;
         int k = n-1;
         while(j<k){
             if (arr[i] + arr[j] + arr[k] == 0){
-                sort();
                 temp.push_back({arr[i],arr[j],arr[k]});
-
+                while(arr[j] == arr[j+1] && j<k){
+                    j++;
+                }
+                j++;
+                while(arr[k-1] == arr[k] && j<k){
+                    k--;
+                }
+                k--;
+            }
+            else if(arr[i] + arr[j] + arr[k] > 0){
+                while(arr[k-1] == arr[k] && j<k){
+                    k--;
+                }
+                k--;
+            }
+            else{
+                while(arr[j] == arr[j+1] && j<k){
+                    j++;
+                }
+                j++;
             }
         }
     }
+    return temp;
 }
 int main(){
     int NumberOfElement;
@@ -28,9 +50,11 @@ int main(){
         cin >> Element;
         arr[i] = Element;
     }
-    Three_Sum(NumberOfElement,arr);
-    for(int i=0;i<NumberOfElement;i++){
-        cout << arr[i] << endl;
+    vector<vector<int>> result = Three_Sum(NumberOfElement,arr);
+    for(const auto& row : result){
+        for(int val:row){
+            cout << val << " ";
+        }
+        cout << endl;
     }
-    return 0;
 }
