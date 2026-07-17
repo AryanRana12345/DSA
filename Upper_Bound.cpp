c7d187be18eace47c04ddb8ff8e028ffd604f5ef
AryanRana12345 <aryan.rana.ug24@nsut.ac.in>
2026-07-17 15:40:45 +0530

    I have also successfully completed the Upper Bound problem in the first try and i have used the code from the lower bound solution as it is just a tweak away from that so it helped me save time but still learn the logic behind it

---
+++ Upper_Bound.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int Upper_Bound(int low,int high, vector<int>& arr,int Target){
+    int index = arr.size();
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(arr[mid] <= Target){
+            low = mid+1;
+        }
+        else{
+            index = mid;
+            high = mid-1;
+        }
+    }
+    return index;
+}
+int main(){
+    int n;
+    cout << "Number of elements: ";
+    cin >> n;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int temp;
+        cout << "Element " << i+1 << ": ";
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    int Target;
+    cout << "Target Number: ";
+    cin >> Target;
+    int low = 0;
+    int high = n-1;
+    cout << "Output: " << Upper_Bound(low,high,arr,Target)+1;
+}
