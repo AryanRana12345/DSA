503489f13d1f8a3197c9c031799f64ecff0b3ad2
AryanRana12345 <aryan.rana.ug24@nsut.ac.in>
2026-08-14 08:22:18 +0530

    I have finally succeeded in completing the the problem of finding the median of two sorted array and i had to face multiple problems while trying to solve it like what about the edge case where either l1 or r1 didn't existed but in the end i was able to solve it.

---
+++ Median_Of_Two_Sorted_Array.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include <climits>
+using namespace std;
+double Median_Of_Two_Sorted_Array(vector<int>& nums1,vector<int>& nums2, int n1, int n2){
+    if(n1 > n2) return Median_Of_Two_Sorted_Array(nums2,nums1,n2,n1);
+    int mid1 = (n1+n2+1)/2;
+    int low = 0;
+    int high = n1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        int mid2 = mid1 - mid;
+        int l1 = INT_MIN;
+        int l2 = INT_MIN;
+        int r1 = INT_MAX;
+        int r2 = INT_MAX;
+        if(mid-1 >=0) l1 = nums1[mid-1];
+        if(mid < n1) r1 = nums1[mid];
+        if(mid2-1 >=0) l2 = nums2[mid2-1];
+        if(mid2 < n2) r2 = nums2[mid2];
+        if(l1 > r2) high = mid-1;
+        else if(l2>r1) low = mid+1;
+        else{
+            if((n1+n2)%2==0) return ((double)max(l1,l2) + min(r1,r2))/2;
+            else return max(l1,l2);
+        }
+    }
+    return 0;
+}
+int main(){
+    int n1;
+    cout << "Size of First Array: ";
+    cin >> n1;
+    vector<int> nums1;
+    for(int i = 1;i <= n1;i++){
+        int temp;
+        cout << "Element " << i << ": ";
+        cin >> temp;
+        nums1.push_back(temp);
+    }
+    int n2;
+    cout << "Size of Second Array: ";
+    cin >> n2;
+    vector<int> nums2;
+    for(int i = 1;i <= n2;i++){
+        int temp;
+        cout << "Element " << i << ": ";
+        cin >> temp;
+        nums2.push_back(temp);
+    }
+    cout << "Output: " << Median_Of_Two_Sorted_Array(nums1,nums2,n1,n2);
+}
