1ba4102bdeebb02a933a20f5897aa5d644575455
AryanRana12345 <aryan.rana.ug24@nsut.ac.in>
2026-09-12 10:34:08 +0530

    I have successfully completed the problem of detecting a cycle in a linked list.

---
+++ Detect_Cycle_In_LL.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+class ListNode{
+    public:
+        int data;
+        ListNode* next;
+        ListNode(int data1,ListNode* next1 = nullptr){
+            data = data1;
+            next = next1;
+        }
+};
+bool Detect_Cycle_In_LL(ListNode* head){
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) return true;
+    }
+    return false;
+}
+int main(){
+    ListNode *head = new ListNode(1);
+    ListNode *second = new ListNode(2);
+    ListNode *third = new ListNode(3);
+    ListNode *fourth = new ListNode(4);
+    ListNode *fifth = new ListNode(5);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = nullptr;
+    cout << Detect_Cycle_In_LL(head);
+}
